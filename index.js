2cbe725ea76190091b5f653d6a5b57a04d9d0fa8
AnandIsCoding <anandkumarj669@gmail.com>
2024-10-11 12:52:49 +0530

    delete and update api created

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+
 const app = express();
 const connectToDb = require("./configs/database")
 const User = require('./models/user')
@@ -16,6 +17,9 @@ connectToDb()
         console.log('Refresh page or wait for sometime')
     })
 
+app.get('/', (req, res) => {
+        res.send('Home page');
+})
 
 app.post('/signup', async(req,res) =>{
     const user = new User(req.body)
@@ -28,10 +32,75 @@ app.post('/signup', async(req,res) =>{
     }
 })
 
-
-app.get('/', (req, res) => {
-    res.send('Home page');
+app.get('/user', async(req, res)=>{
+    const userEmail = req.body.email;
+    try{
+        const allUser = await User.find({email : userEmail})
+        if(allUser.length === 0){
+            res.status(404).send('User not found');
+        }else{
+            res.send(allUser)
+        }        
+    }catch(error){
+        console.log(error)
+        res.status(404).send('Error')
+    }
 })
+
+app.get('/feed', async(req, res) =>{
+
+    try{
+        const allUsers = await User.find({});
+        res.send(allUsers)
+    }catch(error){
+        res.status(404).send('No usrs found');
+        console.log(error)
+    }
+})
+
+//app.delete
+
+app.delete('/delete', async(req, res) =>{
+    const userId = req.body._id;
+    const userAvailable = await User.find({_id : userId})
+    if(userAvailable.length === 0){
+        res.status(404).send('User not found');
+        return;  //To prevent further execution if user not found.
+    }    
+    try{
+        await User.deleteOne({_id : userId})
+        res.send('User deleted successfully');
+
+    }catch(error){
+        console.log(error)
+        res.status(500).send('Try later')
+    }
+})
+
+//update
+app.patch('/user', async (req, res) => {
+    const userId = req.body._id;
+    const dataToBeUpdated = req.body;
+
+    try {
+        // Check if the user exists
+        const isUserAvailable = await User.find({ _id: userId });
+        if (isUserAvailable.length === 0) {
+            return res.status(404).send('User Not Found');
+        }
+        // Update the user data
+        await User.findByIdAndUpdate(userId, dataToBeUpdated, { new: true });
+        res.status(200).send('Updated successfully');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Try sometime later');
+    }
+});
+
+
+
+
+
 
 
 
